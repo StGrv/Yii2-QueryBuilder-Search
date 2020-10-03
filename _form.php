6d2ca4b5a6ae23905fe4b6869fdd2a9e02e9cf45
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-03 13:16:48 +0300

    The Form file

--- _form.php
+++ _form.php
@@ -4,7 +4,8 @@ use yii\widgets\ActiveForm;
 use yii\helpers\Url ;
 
     <?php $form = ActiveForm::begin([
-    'action' => Url::to('@web/categories/index')
+    'action' => Url::to('@web/categories/index') // If we want to navigate to an existing page
+    // 'action' => Url::to('@web/categories/search') // If we want to navigate to a controller Action dynamic page (public function actionSearch())  
     ]); ?>
 
     <input type="text" name="search" placeholder="Search..">
