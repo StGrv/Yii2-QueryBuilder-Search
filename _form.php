4fa82351060b0b7f86db42c776b33b0caa3033ac
StGrv <65493257+StGrv@users.noreply.github.com>
2020-10-03 13:18:29 +0300

    The Form file

--- _form.php
+++ _form.php
@@ -5,7 +5,7 @@ use yii\helpers\Url ;
 
     <?php $form = ActiveForm::begin([
     'action' => Url::to('@web/categories/index') // If we want to navigate to an existing page
-    // 'action' => Url::to('@web/categories/search') // If we want to navigate to a controller Action dynamic page (public function actionSearch())  
+    // 'action' => Url::to('@web/categories/search') // If we want to navigate to a controller Action dynamic page
     ]); ?>
 
     <input type="text" name="search" placeholder="Search..">
@@ -13,3 +13,10 @@ use yii\helpers\Url ;
 
     <?php $form = ActiveForm::end() ; ?>
 ?>
+
+/* 
+In case we want to navigate to a controller Action dynamic page we have to include in the controller the following code:
+    public function actionSearch() {
+        return $this->render('_search') ; // The page we would like to render that contains the search functionality
+    }
+*/
